2f68e353d0d3a64e34dc2c1bee745bc47eb9f90b
w67248 <w67248@student.wsiz.edu.pl>
2022-10-25 13:01:25 +0200

    rozwiązania zadań

---
+++ lab3/zad6.py
@@ -0,0 +1,14 @@
+suma=0
+n=0
+
+while True:
+    b=int(input("Podaj liczbę punktów: "))
+
+    if (b<0 or b>100):
+        break
+    else:
+        suma=suma+b
+        n=n+1
+        continue
+
+print (suma/n)
