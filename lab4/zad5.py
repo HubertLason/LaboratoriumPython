65e5eb29f91b5af7cb29042591f3be73816020fd
w67248 <w67248@student.wsiz.edu.pl>
2022-11-15 13:22:48 +0100

    rozwiązania zadań

--- lab4/zad5.py
+++ lab4/zad5.py
@@ -6,8 +6,8 @@ for i in range(15):
     x=round(random.uniform(1,50),2)
     punkty.append(x)
 
-print (max(punkty))
-print (min(punkty))
+print(max(punkty))
+print(min(punkty))
 
 a=float(input("Podaj liczbę punktów: "))
 
@@ -20,9 +20,24 @@ suma=0
 
 for i in range(15):
     suma+=punkty[i]
-print(suma/15)
+print(f"Średnia: {suma/15}")
 
 powyzej=0
+ponizej=0
+powy=[]
+poni=[]
 
 for i in range(15):
-    if punkty[i]>(suma/15)
+    if punkty[i]>(suma/15):
+        powyzej+=1
+        powy.append(punkty[i])
+    elif punkty[i]<(suma/15):
+        ponizej+=1
+        poni.append(punkty[i])
+
+print(f"Ilość osób powyżej średniej: {powyzej}")
+print(f"Ilość osób poniżej średniej: {ponizej}")
+print(f"Punkty powyżej średniej{powy}")
+print(f"Punkty poniżej średniej{poni}")
+
+
